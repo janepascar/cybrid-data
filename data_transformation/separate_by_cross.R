3f1661a8f9921adabe41652ba9caedda8daad33a
janepascar <34220531+janepascar@users.noreply.github.com>
2018-06-19 16:26:51 -0700

    Add files via upload

---
+++ data_transformation/separate_by_cross.R
@@ -0,0 +1,24 @@
+#!/usr/bin/env Rscript
+# usage Rscript separate_by_cross.R cybrid_transformed.csv
+# Use this to separate the master transformed data into individual csv files based on crosses
+# output: [cross].csv files into working directory
+# must have already run cybrid_transformed.R
+
+packages <- c("dplyr", "stringr")
+lapply(packages, library, character.only = TRUE)
+
+args = commandArgs(trailingOnly = TRUE)
+args2 = str_sub(args, 1, str_length(args)-4)
+dat <- read.csv(args)
+
+dat$fid <- as.factor(dat$fid)
+
+lines <- unique(dat$cross)
+
+for (line in lines) {
+  a <- dat %>%
+    filter(cross == line)
+  name <- paste(line,"", sep = "")
+  assign(name, a)
+  write.csv(name, file =paste(name, ".csv", sep = ""))
+}
